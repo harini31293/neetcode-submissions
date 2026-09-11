c3e000213271c6e48555e76406f063cd3bfbdcc6
Harini H S <75746839+harini31293@users.noreply.github.com>
2026-09-11 07:37:07 +0530

    Add: k-closest-points-to-origin - submission-1

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        minHeap = []
+        for x, y in points:
+            dist = (x**2) + (y**2)
+            print(dist)
+            minHeap.append([dist, x, y])
+        print(minHeap)
+        heapq.heapify(minHeap)
+        print(minHeap)
+        res = []
+        while k >0:
+            dist, x, y = heapq.heappop(minHeap)
+            res.append([x,y])
+            k -=1
+        return res
+
+        
